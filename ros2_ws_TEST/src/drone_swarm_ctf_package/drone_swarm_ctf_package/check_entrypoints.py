516f5ae0a4225a6a11718419c70019bbda4ecb21
Oliver Ekeberg <oliveroekeberg@gmail.com>
2025-12-05 10:49:34 +0100

    Removed unused workspaces

--- ros2_ws_TEST/src/drone_swarm_ctf_package/drone_swarm_ctf_package/check_entrypoints.py
+++
@@ -1,4 +0,0 @@
-import pkg_resources
-eps = list(pkg_resources.iter_entry_points('console_scripts'))
-print('All entry points:')
-[print(f'{ep.name}: {ep.module_name}') for ep in eps if 'drone' in ep.name]
--- run.sh
+++ run.sh
@@ -1,49 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ARG="${1:-d}"   # default is "d"
-
-case "$ARG" in
-  d)
-    docker run -it \
-      --gpus all \
-      -e DISPLAY="$DISPLAY" \
-      -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-      -v "$(pwd)/ros2_ws_TEST:/usr/local/ros2_ws" \
-      webots-drone
-    ;;
-  test_fra_github)
-    docker run -it \
-      --gpus all \
-      --net=host \
-      --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-      -e DISPLAY=$DISPLAY\
-      -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-      -v ~/.Xauthority:/root/.Xauthority:rw \
-      -v "$(pwd)/ros2_ws_test2:/usr/local/ros2_ws" \
-      webots-drone
-    ;;
-
-  oliver_mac)
-
-    # ensure XAUTHORITY is set
-    #: "${XAUTHORITY:=$HOME/.Xauthority}"
-    #touch "$XAUTHORITY"
-
-    docker run -it \
-    --net=host \
-    -e DISPLAY=$DISPLAY \
-    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v "$(pwd)/ros2_ws_test2:/usr/local/ros2_ws" \
-    webots-drone
-    ;;
-
-  *)
-    echo "Invalid argument."
-    echo "Run './run.sh d' for the default setup."
-    echo "Run './run.sh test_fra_github' for the test_fra_github setup. This mount containes the proper swarm structure I found on github"
-    echo "Run './run.sh oliver_mac' for macOS/XQuartz setup."
-    exit 1
-    ;;
-esac
+docker run -it \
+  --gpus all \
+  --net=host \
+  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+  -e DISPLAY=$DISPLAY\
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v ~/.Xauthority:/root/.Xauthority:rw \
+  -v "$(pwd)/ros2_ws:/usr/local/ros2_ws" \
+  webots-drone
