d3cd435901343d3d4388dcb8891fdbeec6335986
Oliver Ekeberg <oliveroekeberg@gmail.com>
2025-12-05 17:42:01 +0100

    Simulation works!!!

--- ros2_ws/start_simulation.sh
+++ ros2_ws/start_simulation.sh
@@ -4,5 +4,4 @@ cd ./ros2_ws/
 source /opt/ros/humble/setup.bash
 colcon build --symlink-install
 source install/local_setup.bash
-ros2 launch mavic_simulation robot_launch.py
-
+nohup ros2 launch mavic_simulation robot_launch.py > output.log 2>&1 &
